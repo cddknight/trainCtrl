a6c18e8052c5d56361910ba01bd8ef2dbaed49d3
Chris Knight <chris.knight@teligent.co.uk>
2019-01-11 14:51:23 +0000

    Fixed version on about

--- src/trainControl.c
+++ src/trainControl.c
@@ -23,6 +23,7 @@
 #include <gtk/gtk.h>
 #include <string.h>
 
+#include "config.h"
 #include "trainControl.h"
 #include "socketC.h"
 
@@ -1164,7 +1165,7 @@ static void aboutCallback (GSimpleAction *action, GVariant *parameter, gpointer 
 	gtk_show_about_dialog (GTK_WINDOW (NULL),
 			"program-name", "Train Control",
 			"version", g_strdup_printf ("%s,\nRunning against GTK+ %d.%d.%d",
-			 "0.0",
+			 VERSION,
 				 gtk_get_major_version (),
 				 gtk_get_minor_version (),
 				 gtk_get_micro_version ()),
