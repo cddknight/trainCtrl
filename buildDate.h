6e6e5c01773d8fa05c36cf45a1d1d7a130ee7b50
Chris Knight <chris@ckfive.theknight.home>
2022-01-25 10:09:03 +0000

    New build date

--- buildDate.h
+++ buildDate.h
@@ -4,7 +4,7 @@
 #ifndef BUILDDATE_H
 #define BUILDDATE_H
 
-static const char *buildDate = "2021/01/14";
-static const char *buildTime = "11:01:41";
+static const char *buildDate = "2022/01/25";
+static const char *buildTime = "10:02:08";
 
 #endif
