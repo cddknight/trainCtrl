6927886bfbdecddc6a77beb846640fadd9be52ec
Chris Knight <chris@theknight.co.uk>
2023-11-22 08:54:24 +0000

    Fixed copyright year

--- src/trainControl.c
+++ src/trainControl.c
@@ -1868,7 +1868,7 @@ static void aboutCallback (GSimpleAction *action, GVariant *parameter, gpointer 
 			"program-name", "Train Control",
 			"version", g_strdup_printf ("Version: %s\nBuilt: %s",
 				 VERSION, buildDate),
-			"copyright", "Copyright © 2018 - 2022 TheKnight",
+			"copyright", "Copyright © 2018 - 2023 TheKnight",
 			"license-type", GTK_LICENSE_LGPL_2_1,
 			"website", "http://www.theknight.co.uk",
 			"comments", "Program to control trains with DCC++.",
